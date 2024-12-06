d3b3a62cdee7e8b6e2581be346483b4ec7e7ef24
Raza-Ur-Rehman <mrazaurrehman12345@gmail.com>
2024-12-07 03:09:08 +0500

    backend

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import { routes } from "./routes/routes.js";
+
+const app = express();
+dotenv.config();
+
+// app.get("/", (req, res) => {
+//   res.status(200).send(index);
+// });
+
+routes.map(({ path, component }) => {
+  return app.get(path, (req, res) => {
+    res.status(200).send(component);
+  });
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ routes/routes.js
@@ -0,0 +1,40 @@
+
+import { home } from "../data/home.js";
+import { Users } from "../data/users.js";
+import { Todos } from "../data/todos.js";
+import { Photos } from "../data/photos.js";
+import { Albums } from "../data/albums.js";
+import { Comments } from "../data/comments.js";
+import { Posts } from "../data/posts.js";
+
+export const routes = [
+    {
+        path: '/',
+        component: home
+
+    },
+    {
+        path: '/posts',
+        component: Posts
+    },
+    {
+        path: '/comments',
+        component: Comments
+    },
+    {
+        path: '/albums',
+        component: Albums
+    },
+    {
+        path: '/photos',
+        component: Photos
+    },
+    {
+        path: '/todos',
+        component: Todos
+    },
+    {
+        path: '/users',
+        component: Users
+    },
+]
